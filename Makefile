9087fd510e2e4321c58b174a77a6b944b91ce215
mli2830 <wzmichael.li@gmail.com>
2015-12-24 00:25:52 -0500

    why is my make not working?

--- Makefile
+++ Makefile
@@ -1,1 +1,18 @@
 #Makefile 
+
+current: target
+
+target:	  covariates.Rout
+	  less covariates.Rout
+############################################
+
+gitroot = ../
+-include local.mk
+-include $(gitroot)/local.mk
+ms = $(gitroot)/makestuff
+
+
+############################################
+
+covariates.Rout: times.RData covariates.R
+		 R CMD BATCH covariates.R
--- covariates.R
+++ covariates.R
@@ -2,13 +2,14 @@ library(dplyr)
 
 threshVL <- 400
 set.seed(2017)
-
-obs <- mutate(group_by(obs, id)
-              , setPoint = rlnorm(1, meanlog=7, sdlog=1)
+print(obs)
+obs <- (obs %>% group_by(id)
+          %>% mutate(setPoint = rlnorm(1, meanlog=7, sdlog=1)
               , VL = rlnorm(n(), meanlog=log(setPoint), sdlog=1)
               , VL = pmax(VL, threshVL)
               , logVL = log(VL)
               , cumVL = cumsum(logVL)
+          )
 )
 
 summary(obs)
